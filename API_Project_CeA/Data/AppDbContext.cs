4261bcb4b670d99a5c65d3398fbf9359354ec9cd
Bruno Santana Lopes <brunolopesg@gmail.com>
2021-10-25 22:51:59 -0300

    Commit (Alteração Model)

---
+++ API_Project_CeA/Data/AppDbContext.cs
@@ -0,0 +1,62 @@
+﻿using API_Project_CeA.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Project_CeA.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+
+        }
+
+
+
+        public DbSet<Produto> Produtos { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.nome)
+                    .HasMaxLength(45);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.tamanho)
+                    .HasMaxLength(4);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.categoria)
+                    .HasMaxLength(45);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.divisao)
+                    .HasMaxLength(15);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.descricao)
+                    .HasMaxLength(250);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.fornecedor)
+                    .HasMaxLength(45);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.imagem)
+                    .HasMaxLength(300);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.usuario_Alteracao)
+                    .HasMaxLength(45);
+           
+
+        }
+    }
+}
---
+++ API_Project_CeA/Migrations/20211024153200_Inicial.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_Project_CeA.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    tamanho = table.Column<string>(type: "varchar(4)", maxLength: 4, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    categoria = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    divisao = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    descricao = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    estoque = table.Column<int>(type: "int", nullable: false),
+                    fornecedor = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    usuario_Alteracao = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    reg_Alteracao = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
